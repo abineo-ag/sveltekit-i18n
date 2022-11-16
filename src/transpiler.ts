685d7d51cb59ae92d46c91e4d06c0e440054f0c3
Christoph Jean-Luc Schneider <kopilot.schneider@protonmail.com>
2022-11-16 22:30:40 +0100

    js regex f4cking succs

--- src/transpiler.ts
+++ src/transpiler.ts
@@ -10,7 +10,7 @@ import { PluginOptions } from './plugin';
 import { Summary, Translation } from './types';
 
 // https://regex101.com/r/duqj7S/1
-const VALID_LANG = new RegExp(/^[a-z]{2,}(-[a-z]{2,})*$/gi);
+const VALID_LANG = /^[a-z]{2,}(-[a-z]{2,})*$/gi; //new RegExp(/^[a-z]{2,}(-[a-z]{2,})*$/gi);
 
 export default function (options: PluginOptions) {
 	const srcDir = path.join(...options.src.split(/[\\\/]+/g));
@@ -32,7 +32,7 @@ export default function (options: PluginOptions) {
 		const entries = fs.readdirSync(srcDir).filter((file) => {
 			if (!file.includes('.json')) return false; // '.jsonc' matches as well
 			const lang = file.split('.json')[0]; // works for '.jsonc' matches as well
-			if (VALID_LANG.test(lang)) return true;
+			if (new RegExp(VALID_LANG).test(lang)) return true;
 			else {
 				console.error(
 					`ignore file '${file}' because '${lang}' is not a valid language code`
