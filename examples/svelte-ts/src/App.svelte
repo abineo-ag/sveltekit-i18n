7f54a68f8df724517a1c923c9e51eee1fb177f7d
Christoph Jean-Luc Schneider <kopilot.schneider@protonmail.com>
2022-11-16 23:24:05 +0100

    yes

--- examples/svelte-ts/src/App.svelte
+++ examples/svelte-ts/src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { setLanguage, availableLanguages, t } from './lib/i18n';
-    setLanguage(availableLanguages[0]);
+	setLanguage(availableLanguages[0]);
 </script>
 
 <main>
-    <p>{$t.people.alice}</p>
-    <p>{$t.people.any('Markus')}</p>
+	<p>{$t.people.alice}</p>
+	<p>{$t.people.any('Markus')}</p>
+	<p>{$t.things.one}</p>
+	<p>{$t.things.mutliple(7, 'Poo Bear')}</p>
 </main>
